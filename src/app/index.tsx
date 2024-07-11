6bc901b6f68ad42cea8572c5b976d2b599a4e8a3
Yasmin358 <amorin.yasmin378@gmail.com>
2024-07-11 17:54:47 -0300

    Criando Botões e Inputs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,48 @@
 import { Text, View, Image } from 'react-native';
 import { Input } from '@/components/input';
+import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native";
+import { colors } from "@/styles/colors";
+import { Button } from '@/components/button';
 
 export default function Index() {
     return ( 
-        <View className="flex-1 items-center justify-center">
-            <Image source={require("@/assets/logo.png")} className="h-8 w-full p-1" resizeMode="contain" />
+        <View className="flex-1 items-center justify-center px-5">
+            <Image style={{ height: 32 }}source={require("@/assets/logo.png")} resizeMode="contain" />
             <Text className="text-zinc-400 font-regular text-center text-lg mt-">Planeje sua proxima viagem!</Text>
-            <View>
-                <Input variant="primary">
-                    <Input.Field />
+            <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
+                <Input>
+                    <MapPin color={colors.zinc[400]} size={20} />
+                    <Input.Field 
+                        placeholder="Para Onde?"
+                    />
                 </Input>
+                <Input>
+                    <IconCalendar color={colors.zinc[400]} size={20} />
+                    <Input.Field 
+                        placeholder="Quando?"
+                    />
+                </Input>
+                <View className="border-b py-3 border-zinc-800">
+                    <Button variant="secondary">
+                        <Button.Title>Alterar local/data</Button.Title>
+                        <Settings2 color={colors.zinc[200]} size={20} />
+                    </Button>
+                </View>
+                <Input>
+                    <UserRoundPlus color={colors.zinc[400]} size={20} />
+                    <Input.Field 
+                        placeholder="Quem vai com você?"
+                    />
+                </Input>
+                <Button>
+                    <Button.Title>Continuar</Button.Title>
+                    <ArrowRight color={colors.rose[900]} size={20} />
+                </Button>
             </View>
+            <Text className="text-zinc-500 font-regular text-center text-base">
+                Ao planejar sua viagem pela plann.er você automaticamente concorda com os nossos{" "} 
+                 <Text className="text-zinc-300 underline">Termos de uso e Politicas de privacidade </Text>
+            </Text>
         </View>
     )
 
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react"
-import { TextInput, View } from "react-native"
+import { TextInput, TextInputProps, View } from "react-native"
 import clsx from "clsx";
+import { colors } from "@/styles/colors";
 
 type Variants = "primary" | "secondary" | "tertiary"
 
@@ -11,18 +12,22 @@ type InputProps = {
 
 function Input({children, variant = "primary"}: InputProps) {
     return <View className={clsx(
-        "w-full h-16 flex-row items-center gap-2",
-        {"h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary"},
-        {"bg-zinc-950": variant === "secondary"},
-        {"bg-zinc-900": variant === "tertiary"},
-    
-    )}>
+        "w-full h-16 flex-row items-center gap-2", {
+        "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
+        "bg-zinc-950": variant === "secondary",
+        "bg-zinc-900": variant === "tertiary",
+    })}>
     {children}
     </View>
 }
 
-function Field() {
-    return <TextInput className="flex-1 text-zinc-100 text-lg font-regular"/>
+function Field({ ...rest }: TextInputProps) {
+    return <TextInput 
+        className="flex-1 text-zinc-100 text-lg font-regular"
+        placeholderTextColor={colors.zinc[400]}
+        cursorColor={colors.zinc[100]}
+        {...rest}
+        />
 }
 
 Input.Field = Field

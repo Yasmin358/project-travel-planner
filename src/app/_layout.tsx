922e3ac38e7e2dc1933f77cf20cadc067c791a8c
Yasmin358 <amorin.yasmin378@gmail.com>
2024-07-11 12:15:59 -0300

    configurando projeto e criando componentes input

---
+++ src/app/_layout.tsx
@@ -0,0 +1,21 @@
+import "@/styles/global.css";
+import { Slot } from "expo-router";
+import { View, StatusBar } from 'react-native';
+import { useFonts, Inter_500Medium, Inter_400Regular, Inter_600SemiBold, Inter_700Bold } from "@expo-google-fonts/inter";
+import Loading from "@/components/loading";
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Inter_500Medium, Inter_400Regular, Inter_600SemiBold, Inter_700Bold 
+    })
+    
+    if(!fontsLoaded) {
+        return <Loading />
+    }
+    
+    return (
+    <View className="flex-1 bg-zinc-950"> 
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        <Slot />
+    </View>
+    )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { Input } from '@/components/input';
 
 export default function Index() {
     return ( 
-        <View>
-            <Text>Hello Word</Text>
+        <View className="flex-1 items-center justify-center">
+            <Image source={require("@/assets/logo.png")} className="h-8 w-full p-1" resizeMode="contain" />
+            <Text className="text-zinc-400 font-regular text-center text-lg mt-">Planeje sua proxima viagem!</Text>
+            <View>
+                <Input variant="primary">
+                    <Input.Field />
+                </Input>
+            </View>
         </View>
     )
 
